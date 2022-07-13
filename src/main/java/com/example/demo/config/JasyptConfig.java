68dc18c64a8530f0a0d97ef4e465c3fc37f56de4
leehosung <hslee@ntels.com>
2022-07-13 15:23:57 +0900

    Jasypt 라이브러리 활용한 yml 암호화

---
+++ src/main/java/com/example/demo/config/JasyptConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${JASYPT_PASSWORD}")
+    private String password;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password); // 암호화할 때 사용하는 키
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+        config.setStringOutputType("base64"); //인코딩 방식
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스d
+        encryptor.setConfig(config);
+        return encryptor;
+
+    }
+
+
+}
